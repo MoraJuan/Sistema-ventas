e7d22f4b64ff2ab236919a1cb4c3f4e2e2a123fe
Juan Cruz Mora <juancruzmora26@gmail.com>
2024-08-21 10:59:45 -0300

    Agregue ver carrito

--- Sistema de venta/controladores/ventana_controller.py
+++
@@ -1,7 +0,0 @@
-def calcular_total(productos):
-    total = 0
-    for producto in productos:
-        total += producto[1]  # Suponiendo que productos es una lista de tuplas (nombre, precio)
-    return total
-
-# Otras funciones de lógica de negocio relacionadas con las ventas
